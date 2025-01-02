842aa4138c48e4ba84c2a58e7cf384b3592ccdc5
HobotYo <277308113@qq.com>
2025-01-02 16:07:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.aboyu.sbs": "https://registry-1.docker.io",
+  "quay.aboyu.sbs": "https://quay.io",
+  "gcr.aboyu.sbs": "https://gcr.io",
+  "k8s-gcr.aboyu.sbs": "https://k8s.gcr.io",
+  "k8s.aboyu.sbs": "https://registry.k8s.io",
+  "ghcr.aboyu.sbs": "https://ghcr.io",
+  "cloudsmith.aboyu.sbs": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
